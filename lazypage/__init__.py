8d970ea1d2bc7ef5d72b7a32f696ce34da9a0f92
taojy123 <taojy123@163.com>
2018-10-31 11:45:22 +0800

    import task

--- lazypage/__init__.py
+++ lazypage/__init__.py
@@ -1,6 +1,6 @@
 
 default_app_config = "lazypage.apps.LazypageConfig"
 
-VERSION = '0.2.6'
+VERSION = '0.2.7'
 
 
--- lazypage/tasks.py
+++ lazypage/tasks.py
@@ -1,10 +1,16 @@
 
-from lazypage.execution import execute_lazy_view
-from lazypage.lazy_celery import celery_app
+from lazypage.settings import lazypage_settings
+
+if lazypage_settings.ASYNC_BY_CELERY:
+
+    from lazypage.execution import execute_lazy_view
+    from lazypage.lazy_celery import celery_app
 
 
-@celery_app.task
-def execute_lazy_task(page_id, view_path, view_class_path, request, *args, **kwargs):
-    execute_lazy_view(page_id, view_path, view_class_path, request, *args, **kwargs)
-    return True
+    @celery_app.task
+    def execute_lazy_task(page_id, view_path, view_class_path, request, *args, **kwargs):
+        execute_lazy_view(page_id, view_path, view_class_path, request, *args, **kwargs)
+        return True
 
+else:
+    execute_lazy_task = None
