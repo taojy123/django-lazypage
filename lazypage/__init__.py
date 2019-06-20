7f01fa42fca94635e41c8b952a5ec2e959cc4f14
taojy123 <taojy123@163.com>
2019-06-20 18:05:02 +0800

    fix import

--- lazypage/__init__.py
+++ lazypage/__init__.py
@@ -1,8 +1,12 @@
-import django
 
 default_app_config = "lazypage.apps.LazypageConfig"
 
-VERSION = '0.3.11'
-DJ_VERSION = '%s.%s.%s' % django.VERSION[:3]
+VERSION = '0.3.12'
+
+try:
+    import django
+    DJ_VERSION = '%s.%s.%s' % django.VERSION[:3]
+except Exception as e:
+    DJ_VERSION = '0.0.0'
 
 
